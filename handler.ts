8cb9b399ca0348092d453e7867371a08dba98d6d
0xayot <101125111+0xayot@users.noreply.github.com>
2022-12-20 09:54:57 +0100

    initial setup

--- handler.ts
+++ handler.ts
@@ -1,6 +1,7 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 
 export const hello: APIGatewayProxyHandler = async (event, context) => {
+
   return {
     statusCode: 200,
     body: JSON.stringify({
@@ -10,3 +11,18 @@ export const hello: APIGatewayProxyHandler = async (event, context) => {
     }),
   };
 };
+
+export const addressProvider :APIGatewayProxyHandler = async (event, context) => {
+  const {email, password} = JSON.parse(event.body)
+  // retrieve address 
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Your address was generated successfully",
+      data: {
+        address: "someAddress"
+      }
+    }),
+  };
+}
+// How do we transfer out the money ?
